011b93f8bb4df002a96d5d0dc4c079c7cb954acb
413831 <alepleins3pp@gmail.com>
2020-07-21 11:23:13 -0300

    Primer commit

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+// Request para servicio captcha
+const request = require("request");
+const path = require('path');
+const port = 8080;
+
+// Express application
+const app = express();
+
+app.use(express.static(__dirname + '/ionicapp'));
+
+// app.listen(process.env.PORT || 8080);
+
+app.get('/*',function(req,res){
+  res.sendFile(path.join(__dirname + '/ionicapp/index.html'));
+});
+
+
+
+app.listen(process.env.PORT || 8080);
+
+
+console.log('Finalizo');
